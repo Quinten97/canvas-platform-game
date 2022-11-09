804d9643316831182207754be75dcc84857dc589
Quinten Favorite <quintenfavorite97@gmail.com>
2022-11-08 21:42:47 -0500

    Feature/add camera to player (#10)
    * Camera tracks to player
    
    * cleanup

--- public/script.js
+++ public/script.js
@@ -1,25 +1,30 @@
-// setInterval(gameLoop, 33); // 33 milliseconds = ~ 30 frames per sec
 import { drawTilemap, helpGetPlatforms, levelSelect } from "./js/Tilemap.js";
-import { input, movePlayer, drawPlayer } from "./js/Player.js";
-//get platform array
+import { input, movePlayer, drawPlayer, getPlayerY } from "./js/Player.js";
+//Get curret levels platforms
 let currentPlatforms = helpGetPlatforms();
 
 //Assigning canvas and context vars
 const canvas = document.getElementById("game-window");
 const context = canvas.getContext("2d");
+
+//add player inputs
 input();
 
 function drawCanvas() {
-  // console.log(player.upKey);
-  //Clear canvas
   context.fillStyle = "white";
-  context.fillRect(0, 0, 720, 1280);
+  context.fillRect(0, 0, 1080, 5000);
+}
+
+function drawAll() {
+  drawCanvas();
   drawTilemap(levelSelect(1), context);
   drawPlayer(context);
 }
-
 //Start game loop
 setInterval(() => {
+  context.save();
+  context.translate(0, getPlayerY() + canvas.height / 2 + 500);
   movePlayer(currentPlatforms);
-  drawCanvas();
+  drawAll();
+  context.restore();
 }, 1000 / 30);
