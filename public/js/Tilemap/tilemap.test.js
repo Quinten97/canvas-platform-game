37cea55101e9584111f7c335e5b0bfcbcddfda18
Quinten Favorite <quintenfavorite97@gmail.com>
2022-11-14 00:57:07 -0500

    fixed testing and added first test

---
+++ public/js/Tilemap/tilemap.test.js
@@ -0,0 +1,14 @@
+import { levelSelect } from "./tilemap.js";
+import { describe, expect, it, toEqual } from "@jest/globals";
+describe("returns platform data based on argument passed in", () => {
+  const levelOneTestData = [
+    [0, 4900, 1080, 1000, 1],
+    [800, 4200, 300, 100, 1],
+    [0, 3500, 250, 100, 1],
+    [600, 2800, 250, 100, 1],
+    [725, 2700, 120, 100, 2],
+  ];
+  it("returns level one platforms when 1 is passed in as the argument", () => {
+    expect(levelSelect(1)).toEqual(levelOneTestData);
+  });
+});
